798ce9127dc1a6effca98ece6248a1e5ccc6dddd
Akif9748 <70021050+Akif9748@users.noreply.github.com>
2021-09-21 23:05:55 +0300

    NODE.JS version

---
+++ vke.js
@@ -0,0 +1,27 @@
+//18, 5 kg/m.'nin altında olanlar: Zayıf
+//18.5 – 24, 9 kg/m. arasında olanlar: Normal kilolu
+//25 – 29, 9 kg/m. arasında olanlar: Fazla kilolu
+//30 – 39, 9 kg/m. arasında olanlar: Obez
+//40 kg/m.'nin üzerinde olanlar: İleri derecede obez (morbid obez), olarak görülür.
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+readline.question(`Boyunuzu giriniz: `, boy => {
+  readline.question(`Kilonuzu giriniz: `, kilo => {
+    var sonuc = kilo / (boy / 100 * boy / 100)
+    if (sonuc < 18.49) {
+      console.log("Vücut kitle endeksiniz: " + sonuc + "\nZayıfsınız.");
+    } else if (sonuc > 18.49 && sonuc < 25) {
+      console.log("Vücut kitle endeksiniz: " + sonuc + "\nNormalsiniz.");
+    } else if (sonuc > 24.99 && sonuc < 30) {
+      console.log("Vücut kitle endeksiniz: " + sonuc + "\nFazla kilolusunuz, sağlığınız için kilo verin.");
+    } else if (sonuc > 29.99 && sonuc < 40) {
+      console.log("Vücut kitle endeksiniz: " + sonuc + "\nObezitesiniz, sağlığınız için kilo verin.");
+    } else if (sonuc > 40) {
+      console.log("Vücut kitle endeksiniz: " + sonuc + "\nİleri obezitesiniz..");
+    }
+    readline.close();
+  });
+});
