6a67ae5c40e393af62fb34b66e622da4c94f6fab
Akif Yüce <70021050+Akif9748@users.noreply.github.com>
2022-03-14 21:13:25 +0300

    Update vke.java

--- vke.java
+++ vke.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
 import java.lang.Math;
 
-public class Vke {
+public class vke {
 
     public static void main(String[] args) {
 
-        
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Boyunuzu giriniz: ");
@@ -13,25 +12,23 @@ public class Vke {
 
         System.out.print("Kilonuzu giriniz: ");
         int kilo = scan.nextInt();
+        if (boy > 5)
+            boy /= 100;
+
+        double sonuc = kilo / Math.pow(boy, 2);// VKE hesaplama
+        System.out.println("Vücut kitle endeksiniz: " + sonuc);
+        if (sonuc < 18.49) // Yanıtlar
+            System.out.println("Zayıfsınız.");
+        else if (sonuc > 18.49 && sonuc < 25)
+            System.out.println("Normalsiniz.");
+        else if (sonuc > 24.99 && sonuc < 30)
+            System.out.println("Fazla kilolusunuz, sağlığınız için kilo verin.");
+        else if (sonuc > 29.99 && sonuc < 40)
+            System.out.println("Obezitesiniz, sağlığınız için kilo verin.");
+        else if (sonuc > 40)
+            System.out.println("İleri obezitesiniz..");
 
         scan.close();
 
-        if (boy > 5) boy /= 100;
-
-    double sonuc = kilo / Math.pow(boy, 2);//VKE hesaplama
-    System.out.println("Vücut kitle endeksiniz: ", sonuc); 
-    if (sonuc < 18.49) {//Yanıtlar
-      System.out.println("Zayıfsınız.");
-    } else if (sonuc > 18.49 && sonuc < 25) {
-      System.out.println("Normalsiniz.");
-    } else if (sonuc > 24.99 && sonuc < 30) {
-      System.out.println("Fazla kilolusunuz, sağlığınız için kilo verin.");
-    } else if (sonuc > 29.99 && sonuc < 40) {
-      System.out.println("Obezitesiniz, sağlığınız için kilo verin.");
-    } else if (sonuc > 40) {
-      System.out.println("İleri obezitesiniz..");
-    }
-        
-        
     }
 }
