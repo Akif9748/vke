200d7a40279aec0483c62c36ad6a10a992054618
Akif Yüce <70021050+Akif9748@users.noreply.github.com>
2022-05-27 03:26:17 +0300

    Temiz kod

--- vke.js
+++ vke.js
@@ -1,25 +1,23 @@
-const readline = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const rl = require("readline").createInterface(process.stdin, process.stdout);
 
-readline.question(`Boyunuzu giriniz: `, boy => {//Boy giriş
-  readline.question(`Kilonuzu giriniz: `, kilo => {//Kilo giriş
-    if (boy > 5) boy /= 100;
+rl.question(`Boyunuzu giriniz: `, boy => {//Boy giriş
+    rl.question(`Kilonuzu giriniz: `, kilo => {//Kilo giriş
+        if (boy > 5) boy /= 100;
 
-    const sonuc = (kilo / (boy ** 2)).toFixed(2);//VKE hesaplama
-    console.log("Vücut kitle endeksiniz: ", sonuc); 
-    if (sonuc < 18.49) {//Yanıtlar
-      console.log("Zayıfsınız.");
-    } else if (sonuc > 18.49 && sonuc < 25) {
-      console.log("Normalsiniz.");
-    } else if (sonuc > 24.99 && sonuc < 30) {
-      console.log("Fazla kilolusunuz, sağlığınız için kilo verin.");
-    } else if (sonuc > 29.99 && sonuc < 40) {
-      console.log("Obezitesiniz, sağlığınız için kilo verin.");
-    } else if (sonuc > 40) {
-      console.log("İleri obezitesiniz..");
-    }
-    readline.close();
-  });
+        const sonuc = kilo / boy ** 2;//VKE hesaplama
+
+        console.log("Vücut kitle endeksiniz:", sonuc.toFixed(2));
+        if (sonuc < 18.49) //Yanıtlar
+            console.log("Zayıfsınız.");
+        else if (sonuc > 18.49 && sonuc < 25)
+            console.log("Normalsiniz.");
+        else if (sonuc > 24.99 && sonuc < 30)
+            console.log("Fazla kilolusunuz, sağlığınız için kilo verin.");
+        else if (sonuc > 29.99 && sonuc < 40)
+            console.log("Obezitesiniz, sağlığınız için kilo verin.");
+        else if (sonuc > 40)
+            console.log("İleri obezitesiniz..");
+
+        rl.close();
+    });
 });
