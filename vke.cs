a66f2c0a5340bf18177afbd61b6e95f6e2829a7f
Akif Yüce <70021050+Akif9748@users.noreply.github.com>
2022-05-27 03:38:34 +0300

    Parantez eksiltme

--- vke.cs
+++ vke.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace vke
 {
@@ -13,32 +13,27 @@ namespace vke
             double kilo = Convert.ToDouble(Console.ReadLine());
 
             if (boy > 5)
-            {
                 boy /= 100;
-            }
+            
 
             double sonuc = kilo / Math.Pow(boy, 2); //VKE hesaplama
             Console.WriteLine("Vücut kitle endeksiniz: " + sonuc);
-            if (sonuc < 18.49)
-            {//Yanıtlar
+            
+            if (sonuc < 18.49)//Yanıtlar
                 Console.WriteLine("Zayıfsınız.");
-            }
+            
             else if (sonuc > 18.49 && sonuc < 25)
-            {
                 Console.WriteLine("Normalsiniz.");
-            }
+                
             else if (sonuc > 24.99 && sonuc < 30)
-            {
                 Console.WriteLine("Fazla kilolusunuz, sağlığınız için kilo verin.");
-            }
+                
             else if (sonuc > 29.99 && sonuc < 40)
-            {
                 Console.WriteLine("Obezitesiniz, sağlığınız için kilo verin.");
-            }
+
             else if (sonuc > 40)
-            {
                 Console.WriteLine("İleri obezitesiniz..");
-            }
+            
         }
     }
 }
