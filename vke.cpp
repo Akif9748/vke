60d2e045ef17811ccf67a9ce288d90fdacbdeba3
Akif Yüce <70021050+Akif9748@users.noreply.github.com>
2022-05-27 03:35:48 +0300

    Locale bloat, temiz kod

--- vke.cpp
+++ vke.cpp
@@ -1,33 +1,31 @@
 #include <iostream>
-#include <clocale>
 #include <math.h>
 
 using namespace std;
 
 int main(){
-    setlocale(LC_ALL, "Turkish");
     float boy, kilo;
 
     cout<<"Boyunuz:"<< endl;
     cin>>boy;
     cout<<"Kilonuz:"<< endl;
     cin>>kilo;
-    if (boy > 5) {
+    if (boy > 5) 
         boy /= 100;
-    }
+    
     double sonuc = kilo / pow(boy,2);//VKE hesaplama
     cout <<"Vucut kitle endeksiniz: "<<sonuc<<endl;
 
-    if (sonuc < 18.49) {//Yanıtlar
+    if (sonuc < 18.49) //Yanıtlar
         cout <<"Zayıfsınız."<<endl;
-    } else if (sonuc > 18.49 && sonuc < 25) {
+    else if (sonuc > 18.49 && sonuc < 25) 
         cout <<"Normalsiniz."<<endl;
-    } else if (sonuc > 24.99 && sonuc < 30) {
+    else if (sonuc > 24.99 && sonuc < 30) 
         cout <<"Fazla kilolusunuz, sagliginiz icin kilo verin."<<endl;
-    } else if (sonuc > 29.99 && sonuc < 40) {
+    else if (sonuc > 29.99 && sonuc < 40) 
         cout <<"Obezitesiniz, sagliginiz icin kilo verin."<<endl;
-    } else if (sonuc > 40) {
+    else if (sonuc > 40) 
         cout <<"Ileri obezitesiniz.."<<endl;
-    }
+    
     return 0;
 }
